4a214703f7549e6b9fbb05bc49140007a31737e4
vajradevam <chaos69480@gmail.com>
2024-08-09 09:19:11 +0530

    newssss

---
+++ 2024-08-09/2/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <source> <destination>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *sourceFile, *destFile;
+    int num1, num2;
+    
+    sourceFile = fopen(argv[1], "r");
+    if (sourceFile == NULL) {
+        printf("Error opening source file.\n");
+        return 1;
+    }
+
+    destFile = fopen(argv[2], "w");
+    if (destFile == NULL) {
+        printf("Error opening destination file.\n");
+        fclose(sourceFile);
+        return 1;
+    }
+
+    while (fscanf(sourceFile, "%d %d", &num1, &num2) == 2) {
+        int result = gcd(num1, num2);
+        fprintf(destFile, "GCD of %d and %d is %d\n", num1, num2, result);
+    }
+
+    fclose(sourceFile);
+    fclose(destFile);
+
+    printf("Done.\n");
+    
+    return 0;
+}
