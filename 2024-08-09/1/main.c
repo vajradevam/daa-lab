3ad0b64bd8a4c91b3782fd0cb5b1dc90515642a5
vajradevam <chaos69480@gmail.com>
2024-08-09 09:18:50 +0530

    news

--- 2024-08-09/1/main.c
+++ 2024-08-09/1/main.c
@@ -12,18 +12,13 @@ void processFile(int n, char *sourceFile, char *destFile) {
     FILE *inFile = fopen(sourceFile, "r");
     FILE *outFile = fopen(destFile, "w");
     
-    if (inFile == NULL || outFile == NULL) {
-        printf("Error opening files!\n");
-        exit(1);
-    }
-    
     int decimal;
     int count = 0;
     
     while (fscanf(inFile, "%d", &decimal) != EOF && count < n) {
         char binary[17] = "0000000000000000";  
         decimalToBinary(decimal, binary, 15);  
-        fprintf(outFile, "The binary equivalent of %d is %s\n", decimal, binary);
+        fprintf(outFile, "The binary of %d is %s\n", decimal, binary);
         count++;
     }
     
@@ -32,22 +27,18 @@ void processFile(int n, char *sourceFile, char *destFile) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != 4) {
         printf("Usage: %s <n> \n", argv[0]);
         return 1;
     }
     
     int n = atoi(argv[1]);
-    char *sourceFile = "inDec.dat";
-    char *destFile = "outBin.dat";
+    char *sourceFile = argv[2];
+    char *destFile = argv[3];
     
     processFile(n, sourceFile, destFile);
     
     FILE *outFile = fopen(destFile, "r");
-    if (outFile == NULL) {
-        printf("Error opening the output file!\n");
-        return 1;
-    }
     
     char line[100];
     while (fgets(line, sizeof(line), outFile)) {
